43d0f16e310587ee79819801708deaea6ed2e1df
noite <noite@DPC2013-4.aluno.local>
2015-08-06 19:53:24 -0300

    Inicio da aula 5

--- src/introjava/Introjava.java
+++ src/introjava/Introjava.java
@@ -16,16 +16,56 @@ public class Introjava {
      * @param args the command line arguments
      */
     
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws java.io.IOException
+    {  
+        char choice;
+        
+        System.out.println("Help on");
+        System.out.println(" 1.if");
+        System.out.println(" 2.switch");
+        System.out.print("Choose one:");
+        choice = (char) System.in.read();
+        
+        System.out.println("\n");
+        
+        switch(choice) {
+            case '1':
+                System.out.println("The if:\n");
+                System.out.println("if(condition)statement:");
+                System.out.println("else statemet;");
+                break;
+            case '2':
+                System.out.println("The switch:\n");
+                System.out.println("switch(expression){");
+                System.out.println(" case constant:");
+                System.out.println("  statement sequence");
+                System.out.println("  break;");
+                System.out.println(" //..");
+                System.out.println("}");
+                break;
+            default:
+                System.out.print("Selection not found.");
+                        
+        }
+        
     
-        aves objPassaro = new aves();
-             String resultado = objPassaro.Comer("mais");
+    }
+        
+        
+        
+        
+//                    AULA DE QUARTA-FEIRA        
+//       ======================================================
+//        aves objPassaro = new aves();
+//             String resultado = objPassaro.Comer("mais");
+//             
+//             if(resultado == "mais"){
+//                 System.out.print("Vou enviar mais comida");
+//             }
+//       ======================================================
              
-             if(resultado == "mais"){
-                 System.out.print("Vou enviar mais comida");
-             }
-             
+        
+        
 //    Scanner teste = new Scanner(System.in);
 //    System.out.println("Informe um valor");
 //    int retorno = teste.nextInt();
@@ -37,6 +77,11 @@ public class Introjava {
 //        }
 //    System.out.println("Valor Informado com sucesso");
 //       
+    
+    
+    
     }
+
+    
     
 }
